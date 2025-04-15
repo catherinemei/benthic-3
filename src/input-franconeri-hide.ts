f501643242ca8e353dd86ae855d9c9ff038c4e65
catherinemei <cath.mei1297@gmail.com>
2025-04-15 11:53:33 -0400

    supplemental material changes

--- src/input-franconeri-hide.ts
+++ src/input-franconeri-hide.ts
@@ -4,14 +4,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "0": {
     id: "0",
     displayName: "Bar chart",
-    description:
-      "Height of Charlie and River at different ages. With axes age and height in inches.",
-    descriptionTokens: {
-      label: "Bar chart",
-      shortDescription: "Height of Charlie and River at different ages.",
-      longDescription:
-        "Height of Charlie and River at different ages. With axes age and height in inches.",
-    },
+    description: "Height of Charlie and River at different ages. With axes age and height in inches.",
     parents: [],
     children: ["1", "2"],
     priority: 0,
@@ -19,12 +12,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "1": {
     id: "1",
     displayName: "Age",
-    description: "Age. Contains 3 groups.",
-    descriptionTokens: {
-      label: "Age",
-      shortDescription: "Contains 3 groups.",
-      longDescription: "Contains 3 groups.",
-    },
+    description: "Contains 3 groups.",
     parents: ["0"],
     children: ["3", "4", "5"],
     priority: 1,
@@ -32,12 +20,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "2": {
     id: "2",
     displayName: "Name",
-    description: "Name of child. Contains 2 groups.",
-    descriptionTokens: {
-      label: "Name",
-      shortDescription: "Contains 2 groups.",
-      longDescription: "Contains 2 groups.",
-    },
+    description: "Contains 2 groups.",
     parents: ["0"],
     children: ["6", "7"],
     priority: 2,
@@ -45,12 +28,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "3": {
     id: "3",
     displayName: "Age 8",
-    description: "Age 8. Contains 2 points.",
-    descriptionTokens: {
-      label: "Age 8",
-      shortDescription: "Age 8",
-      longDescription: "Contains 2 points.",
-    },
+    description: "Contains 2 points.",
     parents: ["1"],
     children: ["8", "9"],
     priority: 1,
@@ -58,12 +36,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "4": {
     id: "4",
     displayName: "Age 10",
-    description: "Age 10. Contains 2 points.",
-    descriptionTokens: {
-      label: "Age 10",
-      shortDescription: "Age 10",
-      longDescription: "Contains 2 points.",
-    },
+    description: "Contains 2 points.",
     parents: ["1"],
     children: ["10", "11"],
     priority: 1,
@@ -71,12 +44,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "5": {
     id: "5",
     displayName: "Age 12",
-    description: "Age 12. Contains 2 points.",
-    descriptionTokens: {
-      label: "Age 12",
-      shortDescription: "Age 12",
-      longDescription: "Contains 2 points.",
-    },
+    description: "Contains 2 points.",
     parents: ["1"],
     children: ["12", "13"],
     priority: 1,
@@ -84,12 +52,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "6": {
     id: "6",
     displayName: "Charlie",
-    description: "Charlie. Contains 3 points.",
-    descriptionTokens: {
-      label: "Charlie",
-      shortDescription: "Charlie",
-      longDescription: "Contains 3 points.",
-    },
+    description: "Contains 3 points.",
     parents: ["2"],
     children: ["8", "10", "12"],
     priority: 1,
@@ -97,12 +60,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "7": {
     id: "7",
     displayName: "River",
-    description: "River. Contains 3 points.",
-    descriptionTokens: {
-      label: "River",
-      shortDescription: "River",
-      longDescription: "Contains 3 points.",
-    },
+    description: "Contains 3 points.",
     parents: ["2"],
     children: ["9", "11", "13"],
     priority: 1,
@@ -110,12 +68,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "8": {
     id: "8",
     displayName: "Charlie at 8",
-    description: "Charlie at 8. 52 inches.",
-    descriptionTokens: {
-      label: "Charlie 8",
-      shortDescription: "52 inches.",
-      longDescription: "52 inches.",
-    },
+    description: "52 inches.",
     parents: ["3", "6"],
     children: [],
     priority: 1,
@@ -123,12 +76,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "9": {
     id: "9",
     displayName: "River at 8",
-    description: "River at 8. 50 inches.",
-    descriptionTokens: {
-      label: "River 8",
-      shortDescription: "50 inches.",
-      longDescription: "50 inches.",
-    },
+    description: "50 inches.",
     parents: ["3", "7"],
     children: [],
     priority: 1,
@@ -136,12 +84,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "10": {
     id: "10",
     displayName: "Charlie at 10",
-    description: "Charlie at 10. 54 inches.",
-    descriptionTokens: {
-      label: "Charlie 10",
-      shortDescription: "54 inches.",
-      longDescription: "54 inches.",
-    },
+    description: "54 inches.",
     parents: ["4", "6"],
     children: [],
     priority: 1,
@@ -149,12 +92,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "11": {
     id: "11",
     displayName: "River at 10",
-    description: "River at 10. 57 inches.",
-    descriptionTokens: {
-      label: "River 10",
-      shortDescription: "57 inches.",
-      longDescription: "57 inches.",
-    },
+    description: "57 inches.",
     parents: ["4", "7"],
     children: [],
     priority: 1,
@@ -162,12 +100,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "12": {
     id: "12",
     displayName: "Charlie at 12",
-    description: "Charlie at 12. 56 inches.",
-    descriptionTokens: {
-      label: "Charlie 12",
-      shortDescription: "56 inches.",
-      longDescription: "56 inches.",
-    },
+    description: "56 inches.",
     parents: ["5", "6"],
     children: [],
     priority: 1,
@@ -175,12 +108,7 @@ export const FranconeriHideHypergraph: Hypergraph = {
   "13": {
     id: "13",
     displayName: "River at 12",
-    description: "River at 12. 55 inches.",
-    descriptionTokens: {
-      label: "River 12",
-      shortDescription: "55 inches.",
-      longDescription: "55 inches.",
-    },
+    description: "55 inches.",
     parents: ["5", "7"],
     children: [],
     priority: 1,
--- src/priority-traversal-types.ts
+++ src/priority-traversal-types.ts
@@ -1,19 +1,9 @@
 export type Id = string;
 
-export type Description = {
-  label: string;
-  color?: string;
-  objectType?: string; // shape, text, image, etc
-  role?: string; // main concept, sub-concept, connector, etc
-  longDescription?: string;
-  shortDescription?: string;
-};
-
 export type RelationNode = {
   id: Id;
   displayName: string;
   description?: string;
-  descriptionTokens?: Description;
   parents: Id[];
   children: Id[];
   priority: number;
@@ -22,37 +12,10 @@ export type RelationNode = {
 export type Hypergraph = {
   [id: Id]: RelationNode;
 };
-
-export type HypergraphWithSibling = {
-  [id: Id]: RelationNodeWithSiblings;
-};
-
-export type RelationNodeWithSiblings = {
-  id: Id;
-  displayName: string;
-  description?: string;
-  descriptionTokens?: Description;
-  parents: Id[];
-  siblings: Id[];
-  children: Id[];
-  priority: number;
-};
-
 export type TraversalOutputProps = {
   nodeGraph: Hypergraph;
   showHypergraph?: boolean;
 };
-
-export type HypergraphNodeWithSiblingsProps = {
-  node: RelationNodeWithSiblings;
-  nodeGraph: HypergraphWithSibling;
-  onNodeClick: (
-    curId: string,
-    newId: string,
-    isFocusedParent?: boolean
-  ) => void;
-};
-
 export type HypergraphNodeProps = {
   node: RelationNode;
   nodeGraph: Hypergraph;
@@ -63,8 +26,4 @@ export type HypergraphNodeProps = {
     newId: string,
     isFocusedParent?: boolean
   ) => void;
-};
-
-export type VisualizerProps = {
-  nodeGraph: Hypergraph;
 };
